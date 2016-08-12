dd0827ff6f7b0bcc8075bb0de377dc737f9668a8
wangdm <wangdm1989@gmail.com>
2016-08-12 19:26:59 +0800

    更项目文件

--- hibernate-tutorial/src/main/java/com/wangdm/tutorial/hibernate/entity/Course.java
+++ hibernate-tutorial/src/main/java/com/wangdm/tutorial/hibernate/entity/Course.java
@@ -12,20 +12,20 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "course")
 public class Course {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name="id", nullable=false, unique=true)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false, unique = true)
     private Integer id;
-	
-	@Column(name="title", nullable=false, length=100)
+
+    @Column(name = "title", nullable = false, length = 100)
     private String title;
-	
-	@ManyToOne
-	@JoinColumn(name="categoryId", nullable=false)
+
+    @ManyToOne
+    @JoinColumn(name = "categoryId", nullable = false)
     private Category category;
 
-	@Column(name="status", nullable=false)
-	private Integer status;
+    @Column(name = "status", nullable = false)
+    private Integer status;
 
 }
